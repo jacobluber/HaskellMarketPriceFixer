6ae4fdc4d500fd1fc883de4d07f259689eb175e8
vha <vha@xena06.cs.trinity.edu>
2013-11-15 12:03:37 -0600

    11-15

--- HaskellMarketPriceFixer/src/Main.hs
+++ HaskellMarketPriceFixer/src/Main.hs
@@ -2,6 +2,17 @@ module Main where
 --Haskell Market Price Fixer for Functional 2322 
 --New comment!
 
+data Util = Integer
 
-main::IO()
-main = undefined
+data Money = Double
+
+data Goods = (Price::Money, Quantity::Integer)
+
+data Buyer = (Util, CashOnHand::Money, [Goods])
+
+data Seller = (MinPrice::Money, [Goods])
+
+data Market = ([Buyer], [Seller])
+
+priceFix :: Market a -> Market a
+
