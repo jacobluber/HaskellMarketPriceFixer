618c964e80cc07e34d3f18592a3d55683c94dcbe
jacobluber <jluber@trinity.edu>
2013-11-16 14:28:36 -0600

    work on market with only 1 good for first milestone

--- HaskellMarketPriceFixer/src/Main.hs
+++ HaskellMarketPriceFixer/src/Main.hs
@@ -2,22 +2,22 @@ module Main where
 --Haskell Market Price Fixer for Functional 2322 
 --New comment!
 
-data Util = Integer
+--data Util = Integer
 
-data Money = Double
+--data Money = Double
 
-data Goods = Price::Money Quantity::Integer
+--data Goods = Price::Money Quantity::Integer
 
-data Buyer = Util CashOnHand::Money [Goods]
+--data Buyer = Util CashOnHand::Money [Goods]
 
-data Seller = MinPrice::Money [Goods]
+--data Seller = MinPrice::Money [Goods]
 
-data Market = [Buyer] [Seller]
+--data Market = [Buyer] [Seller]
 
-priceFix :: Market a -> Market a
+--priceFix :: Market a -> Market a
 
-calcUtilPerMoney :: Seller -> Buyer -> Goods -> [(Goods, Util)]
+--calcUtilPerMoney :: Seller -> Buyer -> Goods -> [(Goods, Util)]
 
-buy :: (Goods, Util) -> Buyer -> Seller -> Market
+--buy :: (Goods, Util) -> Buyer -> Seller -> Market
 
 
---
+++ HaskellMarketPriceFixer/src/PFixer.hs
@@ -0,0 +1,33 @@
+module PFixer where
+
+
+--WORK FROM CLASS TEMPORARILY COMMENTED OUT 
+--data Util = Integer
+--data Money = Double
+--data Goods = Price::Money Quantity::Integer
+--data Buyer = Util CashOnHand::Money [Goods]
+--data Seller = MinPrice::Money [Goods]
+--data Market = [Buyer] [Seller]
+--priceFix :: Market a -> Market a
+--calcUtilPerMoney :: Seller -> Buyer -> Goods -> [(Goods, Util)]
+--buy :: (Goods, Util) -> Buyer -> Seller -> Market
+
+--TEST IMPLEMENTATION FOR MARKET WITH ONLY 1 GOOD 
+data MarketParticipant = Seller Int Int | Buyer Int Int Int deriving (Show, Ord, Eq)
+data Market = Participants [MarketParticipant] deriving (Show, Ord, Eq)
+
+--data Seller a = QP Int Int --(quantity of single good, price per unit of single good)
+--data Buyer b = UMU Int Int Int --(utility from each unit of single good, money to use in market, units held)
+
+val testsellers = [Seller 4 43,Seller 6 67,Seller 2 12] 
+val testbuyers = [Buyer 500 10 0,Buyer 100 50 0,Buyer 2000 500 0] 
+
+priceFix:: [MarketParticipant] -> [MarketParticipant] -> Market
+priceFix buyers sellers result = if head(buyers) == [] then result else 
+                                 let buyerUtility = (\(Buyer x y z) -> x) head(buyers)
+                                     sellerPrices = map (\(Seller x y) -> y) testsellers  
+                                     utilityPerSeller = map (\price -> price/BuyerUtility) sellerPrices                                 
+                                     
+
+
+
