2afa9e0e976f087369c0364bb016f4cd9e8580d7
Irene Fan <irenefan93@gmail.cpm>
2013-11-22 12:27:05 -0600

    Rewrote it in the way that Fogarty wants (of course)

--- HaskellMarketPriceFixer/src/DataType.hs
+++ HaskellMarketPriceFixer/src/DataType.hs
@@ -1,6 +1,21 @@
-data Product = Product {name :: String, price :: Float, quantity :: Integer} der
-iving (Show)
+{-
+data Product = Product {name :: String, price :: Float, quantity :: Integer} deriving (Show)
 data Buyer = Buyer {cash :: Float, info :: [([Char], Float)], totalUtil :: Float, purchased :: [Product]} deriving (Show)
 data Seller = Seller {products :: [Product]} deriving (Show)
 data Market = Market {buyers :: [Buyer], sellers :: [Seller]} | Empty deriving (Show)
---test
+-}
+
+type Name = String
+type Money = Float
+type Price = Float
+type PName = String
+type Utility = Float
+type Quantity = Int
+type Inv {-short for inventory-} = (PName, Quantity, Price) --money is the price paid, usually. You will need to decouple Sellers from prices, at least at first.
+type Want = (PName, Utility)
+type PricingScheme = [(PName, Money)] -- store them separately, or create a different kind of seller for a seller with prices.
+data MarketParticipant = Seller Name [Inv] Money | Buyer Name [Want] Money Utility
+
+data Seller = S {sname :: Name, inventory :: [Inv], profit :: Money}
+data Buyer = B {bname :: Name, wants :: [Want], utility :: Utility} deriving (Show)
+type Market = ([Buyer], [Seller], PricingScheme)
