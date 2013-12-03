b7e758b5b55de66967518264a75cf45525adb2df
jacobluber <jluber@trinity.edu>
2013-12-03 16:26:02 -0600

    final milestone 2

--- HaskellMarketPriceFixer/src/DataType.hs
+++ HaskellMarketPriceFixer/src/DataType.hs
@@ -4,6 +4,7 @@ data MarketParticipant = Seller {sellerId :: String, sellerGoodInfo :: [(String,
 data EndMarket = EndParticipants {totalSellerProfit :: Float, totalBuyerUtility :: Float, participants :: [MarketParticipant]} deriving (Show, Ord, Eq)
 data Market = Participants {marketParticipants :: [MarketParticipant]} | Empty deriving (Show, Ord, Eq)
 data Offer = OfferInfo {sellerInfo :: String, goodInfo :: (String, Float, Float, Float)} | NoSeller deriving (Show, Ord, Eq)
+
 testsellers = [Seller "A" [("a",4,1),("b",7,2),("c",5,3)] 0,Seller "B" [("a",1,3),("b",8,2),("c",2,1)] 0,Seller "C" [("a",0,2),("b",4,2),("c",1,2)] 0] 
 testbuyers = [Buyer "Z" [("a",0,5,2),("b",0,7,1),("c",0,6,1)] 100 0, Buyer "Y" [("a",0,5,4),("b",0,4,2),("c",0,2,1)] 50 0,Buyer "X" [("a",0,8,3),("b",0,10,3),("c",0,8,7)] 250 0] 
 
