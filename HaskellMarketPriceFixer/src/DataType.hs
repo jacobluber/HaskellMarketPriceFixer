7fdced4a34dab09dc82ad2d42921d48de4a2dddf
Irene Fan <irenefan93@gmail.cpm>
2013-12-05 11:49:16 -0600

    Shortened the functions. TODO: 1. stuff in his feedback 2. (maybe) move helper function into a new file.

--- HaskellMarketPriceFixer/src/DataType.hs
+++ HaskellMarketPriceFixer/src/DataType.hs
@@ -1,12 +1,21 @@
 module DataType where
 
-data MarketParticipant = Seller {sellerId :: String, sellerGoodInfo :: [(String, Float, Float)], sellerMoney :: Float} | Buyer { buyerId :: String, buyerGoodInfo :: [(String, Float, Float, Float)], moneyHeld :: Float, accumulatedUtility :: Float} deriving (Show, Ord, Eq)
-data EndMarket = EndParticipants {totalSellerProfit :: Float, totalBuyerUtility :: Float, participants :: [MarketParticipant]} deriving (Show, Ord, Eq)
+data MarketParticipant = Seller {
+        sellerId :: String, sellerGoodInfo :: [(String, Float, Float)], sellerMoney :: Float} 
+        | Buyer { buyerId :: String, buyerGoodInfo :: [(String, Float, Float, Float)], moneyHeld :: Float, accumulatedUtility :: Float} 
+        deriving (Show, Ord, Eq)
+data EndMarket = EndParticipants {
+        totalSellerProfit :: Float, totalBuyerUtility :: Float, participants :: [MarketParticipant]} 
+        deriving (Show, Ord, Eq)
 data Market = Participants {marketParticipants :: [MarketParticipant]} | Empty deriving (Show, Ord, Eq)
 data Offer = OfferInfo {sellerInfo :: String, goodInfo :: (String, Float, Float, Float)} | NoSeller deriving (Show, Ord, Eq)
 
-testsellers = [Seller "A" [("a",4,1),("b",7,2),("c",5,3)] 0,Seller "B" [("a",1,3),("b",8,2),("c",2,1)] 0,Seller "C" [("a",0,2),("b",4,2),("c",1,2)] 0] 
-testbuyers = [Buyer "Z" [("a",0,5,2),("b",0,7,1),("c",0,6,1)] 100 0, Buyer "Y" [("a",0,5,4),("b",0,4,2),("c",0,2,1)] 50 0,Buyer "X" [("a",0,8,3),("b",0,10,3),("c",0,8,7)] 250 0] 
+testsellers = [Seller "A" [("a",4,1),("b",7,2),("c",5,3)] 0,
+        Seller "B" [("a",1,3),("b",8,2),("c",2,1)] 0,
+        Seller "C" [("a",0,2),("b",4,2),("c",1,2)] 0] 
+testbuyers = [Buyer "Z" [("a",0,5,2),("b",0,7,1),("c",0,6,1)] 100 0, 
+        Buyer "Y" [("a",0,5,4),("b",0,4,2),("c",0,2,1)] 50 0,
+        Buyer "X" [("a",0,8,3),("b",0,10,3),("c",0,8,7)] 250 0] 
 
 --Offer SellerStringIdentifier (GoodIdentifier,UnitsOfGoodHeld,PriceChargedForGood,UtilityPerDollarForActiveBuyer) 
 --Seller SellerStringIdentifier [(GoodIdentifier,UnitsOfGoodHeld,PricePaidForGood)] MoneyHeld
@@ -27,7 +36,7 @@ type Price = Float
 type PName = String
 type Utility = Float
 type Quantity = Int
-type Inv {-short for inventory-} = (PName, Quantity, Price) --money is the price paid, usually. You will need to decouple Sellers from prices, at least at first.
+type Inv {-short for inventory-} = (PName, Quantity, Price) 
 type Want = (PName, Utility)
 type PricingScheme = [(PName, Money)] -- store them separately, or create a different kind of seller for a seller with prices.
 data MarketParticipant = Seller Name [Inv] Money | Buyer Name [Want] Money Utility
